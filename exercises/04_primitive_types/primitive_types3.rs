0d406e24f46d706da474993e490c7fc6a13272c7
Shivan Sivakumaran <129010893+shivan-eyespace@users.noreply.github.com>
2025-12-22 07:29:19 +1300

    feat: primitive types

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,8 +1,8 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
-    // let a = ???
+    let a = ['a', 'b'];
 
-    if a.len() >= 100 {
+    if a.len() >= 1 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
